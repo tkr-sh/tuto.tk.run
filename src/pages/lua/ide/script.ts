e0909e4d4c8a4257e147449df9fad25cc7957c1e
tk <49250442+tkr-sh@users.noreply.github.com>
2025-03-01 19:16:28 +0100

    wip

--- src/pages/lua/ide/script.ts
+++ src/pages/lua/ide/script.ts
@@ -1,4 +1,5 @@
 import wasmoon from "wasmoon";
+import {parse} from "luaparse";
 
 const factory = new wasmoon.LuaFactory()
 
@@ -36,3 +37,80 @@ $("#run").on("click", async () => {
     }
 
 })
+
+const TOKENS = {
+    EOF: 1,
+    STRING: 2,
+    KEYWORD: 4,
+    IDENT: 8,
+    NUM: 16,
+    PUNCT: 32,
+    BOOL: 64,
+    NIL: 128,
+    VARARG: 256,
+};
+
+const colorize = () => {
+    const editor = $("#editor");
+    const currentCode = editor.text();
+    const cursorPos = getCursor();
+    console.log(cursorPos);
+    console.log(editor);
+    console.log(editor.getAttribute("selectionStart"));
+
+    const lexer = parse(currentCode, { wait: true });
+    console.log(lexer, currentCode);
+    let totalString = "";
+    let lastIndex = 0;
+
+    for (;;) {
+        const token = lexer.lex();
+
+        if (!token || token.type === TOKENS.EOF) {
+            break;
+        }
+
+        totalString += currentCode.slice(lastIndex, token.range[0])
+        totalString += "<div>"
+        totalString += currentCode.slice(token.range[0], token.range[1])
+        totalString += "</div>"
+
+
+        lastIndex = token.range[1];
+    }
+
+    totalString += currentCode.slice(lastIndex, -1)
+    console.log(totalString);
+
+
+    setCursor(cursorPos);
+    editor.innerHTML = currentCode;
+}
+
+setInterval(colorize, 4000);
+
+
+
+const getCursor = (): number => {
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    const clonedRange = range.cloneRange();
+    clonedRange.selectNodeContents($("#editor"));
+    clonedRange.setEnd(range.endContainer, range.endOffset);
+
+    return clonedRange.toString().length;
+}
+
+const setCursor = (pos: number) => {
+    let el = $("#editor");
+
+    let setpos = document.createRange();
+    let set = window.getSelection();
+
+    setpos.setStart(el.childNodes[0], pos);
+    setpos.collapse(true);
+    set.removeAllRanges();
+    set.addRange(setpos);
+
+    el.focus();
+}
